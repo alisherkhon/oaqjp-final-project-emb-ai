04020ed7bf0271720a95f23f4341505992688c57
Alibek <komorebi26022000@gmail.com>
2025-02-18 01:23:50 -0500

    modified emotion detector to print needed output as a dictionary

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,35 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text
+    
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    # Extract required emotions
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    # Store in dictionary
+    output_dict = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    # Determine dominant emotion
+    highest_emotion_key = max(output_dict, key=output_dict.get)
+    output_dict['dominant_emotion'] = highest_emotion_key
+
+    return output_dict
+
